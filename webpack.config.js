e660b25f76268e6c86caab39505929678b9db7e2
New Future (Weilai LIU) <New.Future@microsoft.com>
2018-08-22 21:44:03 +0800

    latest version

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
 
@@ -7,61 +8,58 @@ module.exports = {
 
     output: {
         filename: 'logger.js',
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, 'dist'),
     },
 
     resolve: {
-        extensions: ['.webpack.js', '.web.js', '.ts', '.js']
+        extensions: ['.ts', '.js']
     },
 
     module: {
-        loaders: [
-            { test: /\.ts$/, loader: 'ts-loader' }
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                options: {
+                    allowTsInNodeModules: true,
+                },
+                // include: __dirname,
+                // exclude: /node_modules/
+            }
         ]
     },
 }
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports.devtool = '#source-map'
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            mangle: {
-                // mangle options
-                reserved: [],
-                toplevel: true,
-                properties: {
-                    reserved: [],
-                    // regex: /.*/,
-                    // mangle property options
+    module.exports.mode = "production"
+    module.exports.devtool = '#source-map';
+    module.exports.optimization = {
+        minimizer: [
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                    output: {
+                        comments: false
+                    },
+                    compress: {
+                        properties: true,
+                        loops: true,
+                        sequences: true,
+                        dead_code: true,
+                        evaluate: true,
+                        conditionals: true,
+                        comparisons: true,
+                        booleans: true,
+                        drop_debugger: true,
+                        if_return: true,
+                        join_vars: true,
+                        unused: true,
+                        // 内嵌定义了但是只用到一次的变量
+                        collapse_vars: true,
+                        // 提取出出现多次但是没有定义成变量去引用的静态值
+                        reduce_vars: true,
+                    }
                 }
-            },
-            compress: {
-                properties: true,
-                loops: true,
-                sequences: true,
-                dead_code: true,
-                evaluate: true,
-                conditionals: true,
-                comparisons: true,
-                booleans: true,
-                drop_debugger: true,
-                if_return: true,
-                join_vars: true,
-                unused: true,
-                // 内嵌定义了但是只用到一次的变量
-                collapse_vars: true,
-                // 提取出出现多次但是没有定义成变量去引用的静态值
-                reduce_vars: true,
-            }
-        }),
-        new webpack.LoaderOptionsPlugin({
-            minimize: true
-        })
-    ])
+            }),
+        ]
+    };
 }
